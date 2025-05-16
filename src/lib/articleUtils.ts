1ffff20d28c7685ce62ad3aa09229da83a7c82f2
Yohei Shiina <ysys.j.0125@gmail.com>
2025-05-16 11:24:46 +0900

    feat: Add articles configuration and implement pagination for article fetching

--- src/lib/articleUtils.ts
+++ src/lib/articleUtils.ts
@@ -1,7 +1,15 @@
 
 import { type PrismaClient, type Article } from '@root/generated/prisma/client';
 
+import articlesConfig from "@root/config/articles.json";
+const ITEMS_PER_PAGE = articlesConfig.articlesPerPage;
+
 export type FetchArticleResult = (Omit<Article, 'content' | 'created_at' | 'updated_at'> & { published_at: Date })[];
+/**
+ * Fetch all published articles
+ * @param prisma 
+ * @returns 
+ */
 export const fetchArticles = async (prisma: PrismaClient): Promise<FetchArticleResult> => {
   const articles = await prisma.article.findMany({
     omit: {
@@ -17,8 +25,45 @@ export const fetchArticles = async (prisma: PrismaClient): Promise<FetchArticleR
       published_at: article.published_at as Date,
     }));
 }
+
+/**
+ * Sort articles by published date in ascending order
+ * @param articles 
+ * @returns 
+ */
 export const sortArticlesByPublishedDateDesc = (articles: FetchArticleResult): FetchArticleResult => {
   return articles.sort((a, b) => {
     return b.published_at.getTime() - a.published_at.getTime(); // Sort in descending order
   });
 };
+
+
+export const fetchPageCount = async (prisma: PrismaClient): Promise<number> => {
+  const count = await prisma.article.count({
+    where: { published_at: { not: null } },
+  });
+  return Math.ceil(count / ITEMS_PER_PAGE);
+}
+
+/**
+ * Fetch articles by page
+ * @param prisma 
+ * @param page 
+ * @returns 
+ */
+export const fetchArticlesByPage = async (prisma: PrismaClient, page: number): Promise<FetchArticleResult> => {
+  const skip = (page - 1) * ITEMS_PER_PAGE;
+  const articles = await prisma.article.findMany({
+    where: { published_at: { not: null, } },
+    omit: {
+      content: true,
+      created_at: true,
+      updated_at: true,
+    },
+    skip,
+    take: ITEMS_PER_PAGE,
+    orderBy: { published_at: 'desc' },
+
+  })
+  return articles.map((_) => ({ ..._, published_at: _.published_at as Date })) // Cast published_at to Date
+}
