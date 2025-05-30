78d7cd9b0fc9124403284fe71d4744566b0f659f
Yohei Shiina <ysys.j.0125@gmail.com>
2025-05-31 01:28:44 +0900

    fix: Update articles configuration and integrate topArticlesCount in the Home component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,10 @@ import prisma from "@/lib/prisma";
 import { fetchArticles } from "@/lib/articleUtils";
 import ArticleList from "@/components/ArticleList";
 import TitleDivider from "@/components/TitleDivider";
+import Crousell from "@/components/Crousell";
+
+import articlesConfig from "@root/config/articles.json";
+const topArticlesCount = articlesConfig.topArticlesCount;
 
 export default async function Home() {
   const articles = await fetchArticles(prisma);
@@ -12,7 +16,8 @@ export default async function Home() {
   return (
     <main className="space-y-4">
       <TitleDivider title="新着記事" />
-      <ArticleList articles={articles.slice(0, 10)} />
+      <Crousell />
+      <ArticleList articles={articles.slice(0, topArticlesCount)} />
       <div className="flex justify-center">
         <button className="btn btn-primary">
           <Link href={`${ARTICLES_PAGE_PREFIX}/1`}>すべての記事を見る</Link>
